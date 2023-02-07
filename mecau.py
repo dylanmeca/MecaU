9034568c4916bdfa0d120c0d1962549e10508455
Dylan Meca <55631040+dylanmeca@users.noreply.github.com>
2023-02-07 14:34:05 -0500

    Update mecau.py

--- mecau.py
+++ mecau.py
@@ -1,21 +1,21 @@
 import os
 import hashlib
 import requests
+import argparse
 
 def is_infected(file):
-    # Calcula el hash del archivo # Calculate the hash of the file
-    print("[*] Scanning file....")
+    # Calcula el hash del archivo
+    print("[*] Escaneando archivo....")
     hasher = hashlib.md5()
     with open(file, 'rb') as f:
         buf = f.read()
         hasher.update(buf)
     file_hash = hasher.hexdigest()
 
-    # Verifica si el hash coincide con el del virus conocido # Check if the hash matches that of the known virus
+    # Verifica si el hash coincide con el del virus conocido
     print("[*] El hash del archivo es: ", file_hash)
-    response = requests.get('https://dylanmeca.github.io/MecaU/hashdb.txt')
+    response = requests.get('http://127.0.0.1/hashdb.txt')
     hash_db = response.text.splitlines()
-    hash_db = list(hash_db)
     if file_hash in hash_db:
         return True
     else:
@@ -26,18 +26,20 @@ def scan_directory(directory):
         for file in files:
             file_path = os.path.join(root, file)
             if is_infected(file_path):
-                # Elimina el archivo infectado # Delete the infected file
+                # Elimina el archivo infectado
                 print("[*] ¡Alerta! Se detecto malware")
                 user = input("[*] Deseas eliminar el malware? (y/n) ")
                 if user == "y":
                     print("[*] Eliminando malware...")
-                    #os.remove(file_path)
                     os.system(f"scrub -p dod {file_path} && shred -zun 10 -v {file_path}")
                     print(f'[*] Eliminado {file_path}')
             else:
                 print("[*] No se encontro malware")
 
-# Ejecuta el escaneo en la siguiente carpeta # Run the scan in the following folder
-#scan_directory('/home/use/directory')
-user = input("[*] Escribe la ruta de la carpeta: ")
-scan_directory(user)
+# Añade un argumento de línea de comandos para especificar el directorio a escanear
+parser = argparse.ArgumentParser(description='Escanear un directorio para detectar malware')
+parser.add_argument('-d', '--directory', required=True, help='Directorio a escanear')
+args = parser.parse_args()
+
+# Ejecuta el escaneo en el directorio especificado
+scan_directory(args.directory)
